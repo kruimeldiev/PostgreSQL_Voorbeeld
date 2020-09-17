6c8e35c6ac5c94fff0c09a70962e94e81c4b1d04
Casper Daris <casperdaris@msn.com>
2020-09-17 20:09:53 +0200

    Comments toegevoegd en ik heb functies toegevoegd

--- src/main/java/org/example/Database/DatabaseHelper.java
+++ src/main/java/org/example/Database/DatabaseHelper.java
@@ -8,20 +8,74 @@ public class DatabaseHelper {
 
         try {
 
+            // Hiermee wordt de driver geladen door het programma
+            // Deze stap is niet (meer) verplicht, maar was in Java 1.6 en eerder wel nodig
             Class.forName("org.postgresql.Driver");
 
+            // Aanmaken van de connectie met de database met behulp van de URL, database naam en database password
             Connection conn = DriverManager.getConnection(DataKeys.CONN_URL, DataKeys.CONN_USER, DataKeys.CONN_PASS);
 
+            // Testen of de connectie succesvol is
             if (conn != null) {
                 System.out.println("Connectie gemaakt");
             } else {
                 System.out.println("Error bij connectie maken");
             }
 
+            // Sluiten van de connectie
             conn.close();
 
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println(e);
+        }
+
+    }
+
+    public static void TableMaken(String tableNaam) {
+
+        try {
+
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection(DataKeys.CONN_URL, DataKeys.CONN_USER, DataKeys.CONN_PASS);
+
+            String query = "CREATE TABLE IF NOT EXISTS " + tableNaam + "(naam_id SERIAL PRIMARY KEY, naam varchar(32) NOT NULL, leeftijd int4)";
+
+            Statement stat = conn.createStatement();
+
+            stat.executeUpdate(query);
+
+            System.out.println("Nieuwe table aangemaakt");
+
+            conn.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void NamenOphalen() {
+
+        try {
+
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection(DataKeys.CONN_URL, DataKeys.CONN_USER, DataKeys.CONN_PASS);
+
+            String query = "SELECT * FROM namen LIMIT 100";
+
+            Statement stat = conn.createStatement();
+
+            ResultSet result = stat.executeQuery(query);
+
+            while (result.next()) {
+                String naam = result.getString("naam");
+                int leeftijd = result.getInt("leeftijd");
+                System.out.println(naam + " " + leeftijd);
+            }
+
+            conn.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
         }
 
     }
--- src/main/java/org/example/GUI.java
+++ src/main/java/org/example/GUI.java
@@ -1,22 +1,45 @@
 package org.example;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import org.example.Database.DatabaseHelper;
 
 public class GUI {
 
-    private Button knop;
+    private Button connectieTestButton, tableMakenButton, namenOphalenButton;
+    private TextField tableNaamTextField;
 
     public GUI(GridPane view) {
 
-        knop = new Button("Klik hier voor connectie");
-        knop.setOnAction(event -> {
+        connectieTestButton = new Button("Connectie test");
+        connectieTestButton.setOnAction(event -> {
             DatabaseHelper.ConnectieMaken();
         });
 
-        view.add(knop, 0, 0);
+        tableNaamTextField = new TextField();
+
+        tableMakenButton = new Button("Table aanmaken");
+        tableMakenButton.setOnAction(event -> {
+            if (!tableNaamTextField.getText().isEmpty()) {
+                DatabaseHelper.TableMaken(tableNaamTextField.getText());
+            }
+        });
+
+        namenOphalenButton = new Button("Namen printen");
+        namenOphalenButton.setOnAction(event -> {
+            DatabaseHelper.NamenOphalen();
+        });
+
         view.setPrefSize(480, 360);
+        view.add(connectieTestButton, 0, 0);
+        view.add(tableNaamTextField, 0,1);
+        view.add(tableMakenButton, 1, 1);
+        view.add(namenOphalenButton, 0, 2);
+        view.setHgap(12);
+        view.setVgap(12);
+        view.setPadding(new Insets(20, 20, 20, 20));
     }
 
 }
